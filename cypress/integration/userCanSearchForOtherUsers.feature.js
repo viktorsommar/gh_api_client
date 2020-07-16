b9540c07a83b6ba46e1719d645b6b0c216d8a673
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-16 10:57:37 +0200

    Adding a test to search for user. Adding axios. Tests failing at search, now attempting to fix that.

---
+++ cypress/integration/userCanSearchForOtherUsers.feature.js
@@ -0,0 +1,16 @@
+describe("User can search for other users on Github", () => {
+  before(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/search/users",
+      response: "fixture:example.json"
+    })
+    cy.visit("/")
+    cy.get("input").type("barack")
+    cy.get("search").click()
+  })
+  it("user can see results", () => {
+    cy.get("items-id-681626")
+  })
+})
